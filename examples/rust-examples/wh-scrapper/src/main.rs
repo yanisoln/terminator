f07a4bbad4f45fc7c3ff1685f8e07ca063b3a23a
tribhuwan-kumar <tribhuwankumar725275@gmail.com>
2025-05-20 20:57:31 +0530

    fix: v0

--- examples/rust-examples/wh-scrapper/src/main.rs
+++ examples/rust-examples/wh-scrapper/src/main.rs
@@ -1,8 +1,9 @@
-use std::{hash::Hash, thread};
-use std::time::Duration;
-
+// example for scrapping whatsapp
 use tracing::Level;
-use terminator::{platforms, AutomationError, Desktop, Selector};
+use std::fs::File;
+use std::io::Write;
+use serde_json::json;
+use terminator::{platforms, AutomationError, Selector};
 
 #[tokio::main]
 async fn main() -> Result<(), AutomationError> {
@@ -17,21 +18,20 @@ async fn main() -> Result<(), AutomationError> {
 
     std::thread::sleep(std::time::Duration::from_millis(500));
 
-    // when the element is name is blank we can get its parent element id then we can retrive the children id
-    // we can also create a Selector with the role (control_type) with name on it
     let ch_list = Selector::Role {
         role: "list".to_string(),
         name: Some("Chats list".to_string()),
     };
 
-    let grp_per = Selector::Role { 
+    let grp_person = Selector::Role { 
         role: "listitem".to_string(), 
-        name: Some("vivek bhaiya".to_string()),
+        name: Some("hero".to_string()),        // replace with the person's name, you wanna scrap the chat
     };
 
+
     let ch_list_ele = engine.find_element(&ch_list, Some(&wh_root), None)?;
-    println!("CHAT ELE: {:#?}", ch_list_ele);
-    let grp_per_ele = engine.find_element(&grp_per, Some(&ch_list_ele), None)?;
+    println!("CHAT ELE: {:?}", ch_list_ele);
+    let grp_per_ele = engine.find_element(&grp_person, Some(&ch_list_ele), None)?;
 
     grp_per_ele.click()?;
     std::thread::sleep(std::time::Duration::from_millis(200));
@@ -42,10 +42,37 @@ async fn main() -> Result<(), AutomationError> {
     };
 
     let messages_list = engine.find_element(&msg_list, Some(&wh_root), None)?;
+    println!("Messages list, {:?}", messages_list);
 
-    // we might need a algo for determining the text depth
-    let child_mes = messages_list.text(100);
-    println!("TEXT: {:#?}", child_mes);
+    let msg_list_items = Selector::Role { 
+        role: "listitem".to_string(), 
+        name: None,
+    };
+
+    let messages_list_items = engine.find_elements(&msg_list_items, Some(&messages_list), None, Some(usize::MAX))?; // messages_list as root
+    println!("message list {:?}", messages_list_items);
+
+    let msg_text = Selector::Role { 
+        role: "text".to_string(), 
+        name: None,
+    };
+
+    let mut collected_texts = Vec::new();
+    for messages_list_item in messages_list_items {
+        let messages_text = engine.find_element(&msg_text, Some(&messages_list_item), None)?; // messages_list_items as root
+        println!("MESSAGE TEXT: {:?}", messages_text.name());            // the name of the first text from list item would be the message
+        if let Some(text) = messages_text.name() {
+            collected_texts.push(text.clone());
+        }
+    }
+
+    let json_data = json!({ "messages": collected_texts });
+    let mut file = File::create("messages.json").expect("failed json");
+    file.write_all(json_data.to_string().as_bytes()).expect("failed to write json");
+
+    for text in &collected_texts {
+        println!("{}", text);
+    }
 
     Ok(())
 }
